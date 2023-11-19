809ea29979f8a4af439457efc78a1d13b8c5c75f
Mircea Scarlatescu <scarlatescu.mircea8@gmail.com>
2023-11-19 02:33:34 +0200

    Added hour and minutes wrapping

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,32 @@ const MINUTES_IN_HOUR: i32 = 60;
 const HOURS_IN_DAY : i32 = 24;
 impl Clock {
     fn new(hours: i32, minutes: i32) -> Self {
-        Self {
-            hours,
-            minutes,
+        let mut new_clock : Clock = Clock{
+            hours : 0,
+            minutes : 0,
+        };
+        new_clock = new_clock.add_minutes(minutes);
+        
+        if new_clock.hours + hours >= HOURS_IN_DAY
+        {
+            new_clock.hours = (new_clock.hours + hours) +HOURS_IN_DAY;
+        } else {
+            new_clock.hours = new_clock.hours + hours;
         }
+
+        new_clock
     }
 
     fn add_minutes(&self, minutes: i32) -> Self {
         let minutes = self.minutes + minutes;
-        let new_minutes = minutes % MINUTES_IN_HOUR;
+        let new_minutes;
+        if minutes >= MINUTES_IN_HOUR
+        {
+            new_minutes = minutes % MINUTES_IN_HOUR;
+        } else {
+            new_minutes = minutes;
+        }
+
         let new_hour = self.hours + (minutes / MINUTES_IN_HOUR) % HOURS_IN_DAY;
         
         Self{
@@ -48,4 +65,10 @@ fn main() {
 
     let clock = Clock::new(5, 32).add_minutes(1500);
     assert_eq!(clock.to_string(), "06:32");
+
+    assert_eq!(Clock::new(2, 4322), Clock::new(2, 2));
+
+    assert_eq!(Clock::new(3, -20), Clock::new(2, 40));
+
+    assert_eq!(Clock::new(5, -1490), Clock::new(4, 10));
 }
